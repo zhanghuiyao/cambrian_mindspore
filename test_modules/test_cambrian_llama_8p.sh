8f324709c04dfe53faff64785a4c4f029f79d460
zhanghuiyao <1814619459@qq.com>
2024-08-12 19:06:50 +0800

    1

--- test_modules/test_cambrian_llama_8p.sh
+++ test_modules/test_cambrian_llama_8p.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+#export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export ASCEND_RT_VISIBLE_DEVICES=4,5,6,7
 export MS_ENABLE_NUMA=0
 export MS_MEMORY_STATISTIC=1
 export GLOG_v=2
@@ -12,10 +13,10 @@ export MS_DEV_RUNTIME_CONF="synchronize:True"
 task_name="test_cambrian_llama_8p"
 output_dir=$task_name
 
-master_port=9003
+master_port=9004
 
 
-msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=$master_port --log_dir=$output_dir \
+msrun --bind_core=True --worker_num=4 --local_worker_num=4 --master_port=$master_port --log_dir=$output_dir \
 python test_modules/test_cambrian_llama.py \
     --device_target Ascend \
     --is_distribute True \
