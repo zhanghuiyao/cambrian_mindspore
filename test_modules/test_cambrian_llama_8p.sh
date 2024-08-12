148c0f2069c23a6d032eb76f1ccbe2a47fdd616a
zhanghuiyao <1814619459@qq.com>
2024-08-12 18:23:01 +0800

    1

--- test_modules/test_cambrian_llama_8p.sh
+++ test_modules/test_cambrian_llama_8p.sh
@@ -12,7 +12,7 @@ export MS_DEV_RUNTIME_CONF="synchronize:True"
 task_name="test_cambrian_llama_8p"
 output_dir=$task_name
 
-master_port=9002
+master_port=9003
 
 
 msrun --bind_core=True --worker_num=8 --local_worker_num=8 --master_port=$master_port --log_dir=$output_dir \
