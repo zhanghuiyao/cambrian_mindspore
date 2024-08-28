e3845f8ac0759da3e16d17d4a2009620f12e53c2
zhanghuiyao <1814619459@qq.com>
2024-08-28 11:02:58 +0800

    1

--- test_modules/test_llama3_modules.py
+++ test_modules/test_llama3_modules.py
@@ -29,7 +29,9 @@ def _get_llama_config(pretrained_model_name_or_path):
     return config
 
 
-def test_llama3_attention(model_path: str):
+def test_llama3_attention(args):
+    model_path = args.model_path
+    layer_idx = args.layer_idx
 
     print(f"=====> test_llama3:")
     print(f"=====> Building model...")
@@ -43,7 +45,7 @@ def test_llama3_attention(model_path: str):
     )
 
     config = _get_llama_config(model_path)
-    model = LlamaAttention(config, layer_idx=0)
+    model = LlamaAttention(config, layer_idx=layer_idx)
 
     print(f"=====> Building model done.")
 
@@ -53,24 +55,32 @@ def test_llama3_attention(model_path: str):
     print(f"=====> Done.")
 
 
-def test_llama3_decoder_layer(model_path: str):
+def test_llama3_decoder_layer(args):
+    model_path = args.model_path
+    layer_idx = args.layer_idx
 
     print(f"=====> test_llama3:")
     print(f"=====> Building model...")
 
     s_time = time.time()
 
+    # temp_data = dict(
+    #     hidden_states=Tensor(np.random.randn(1, 2048, 4096), dtype=ms.float32),
+    #     attention_mask=Tensor(np.full((1, 1, 2048, 2048), -65504), dtype=ms.float32),
+    #     position_ids=Tensor(np.arange(0, 2048, 1)[None], ms.int32),
+    # )
     temp_data = dict(
-        hidden_states=Tensor(np.random.randn(1, 2048, 4096), dtype=ms.float32),
-        attention_mask=Tensor(np.full((1, 1, 2048, 2048), -65504), dtype=ms.float32),
-        position_ids=Tensor(np.arange(0, 2048, 1)[None], ms.int32),
+        hidden_states=Tensor(np.load("./hidden_states_in_1.npy"), dtype=ms.float32),
+        attention_mask=Tensor(np.load("./attention_mask.npy"), dtype=ms.float32),
+        position_ids=Tensor(np.load("./position_ids.npy"), ms.int32),
     )
 
     config = _get_llama_config(model_path)
-    model = LlamaDecoderLayer(config, layer_idx=0)
+    model = LlamaDecoderLayer(config, layer_idx=layer_idx)
 
     print(f"=====> Building model done.")
 
+    breakpoint()
     result = model(**temp_data)
 
     print(f"=====> output result: {result}, time cost: {time.time() - s_time:.2f}s")
@@ -82,16 +92,23 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="test")
     parser.add_argument("--model_path", type=str, default="./cambrian/hf-configs/nyu-visionx-cambrian-8b")
+    parser.add_argument("--checkpoint_path", type=str, default="./")
+    parser.add_argument("--layer_idx", type=int, default=1)
+    parser.add_argument("--ms_mode", type=int, default=1)
     parser.add_argument("--jit_level", type=str, default="O0")
     parser.add_argument("--module_name", type=str, default="decoder_layer", choices=["attention", "decoder_layer"])
     args, _ = parser.parse_known_args()
 
-    # ms.set_context(mode=ms.PYNATIVE_MODE, device_target="CPU", pynative_synchronize=True)
-    ms.set_context(mode=ms.GRAPH_MODE, device_target="Ascend", jit_config={"jit_level": args.jit_level}, deterministic="ON")
+    if args.ms_mode == 0:
+        ms.set_context(mode=ms.GRAPH_MODE, device_target="Ascend", jit_config={"jit_level": args.jit_level},
+                       deterministic="ON")
+    else:
+        ms.set_context(mode=ms.PYNATIVE_MODE, device_target="Ascend", pynative_synchronize=True,
+                       deterministic="ON")
 
     if args.module_name == "attention":
-        test_llama3_attention(args.model_path)
+        test_llama3_attention(args)
     elif args.module_name == "decoder_layer":
-        test_llama3_decoder_layer(args.model_path)
+        test_llama3_decoder_layer(args)
     else:
         raise ValueError
