4623ae82d913518977fa9e713baad77aa871eb98
zhanghuiyao <1814619459@qq.com>
2024-08-28 23:30:33 +0800

    1

--- cambrian/mindspore_adapter/amp.py
+++ cambrian/mindspore_adapter/amp.py
@@ -34,6 +34,7 @@ def auto_mixed_precision(network, amp_level="O0", dtype=ms.float16):
     elif amp_level == "O1":
         raise NotImplementedError
     elif amp_level == "O2":
+        from cambrian.transformers.models.llama.modeling_llama import LlamaAttention  # breakpoint()
         _auto_black_list(
             network,
             AMP_BLACK_LIST + [
@@ -48,7 +49,8 @@ def auto_mixed_precision(network, amp_level="O0", dtype=ms.float16):
                 nn.AvgPool1d,
                 nn.AvgPool2d,
                 nn.AvgPool3d,
-                nn.CrossEntropyLoss
+                nn.CrossEntropyLoss,
+                LlamaAttention
             ],
             dtype,
         )
