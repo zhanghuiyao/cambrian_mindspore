eed09abe8270907e96e33d31846f135429bb65dd
zhanghuiyao <1814619459@qq.com>
2024-10-30 16:19:32 +0800

    debug llama3-only 4

--- test_modules/test_llama3_gen.py
+++ test_modules/test_llama3_gen.py
@@ -142,6 +142,74 @@ def run_llama3_generate(args):
         print("=" * 100)
 
 
+def run_llama3_generate_2(args):
+
+    print(f"=====> test_llama3_generate:")
+    print(f"=====> Building model...")
+
+    s_time = time.time()
+
+    from cambrian.model.language_model.cambrian_llama import CambrianLlamaForCausalLM
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_path)
+    model = CambrianLlamaForCausalLM.from_pretrained(args.model_path, use_flash_attention_2=args.use_fa)
+    model.model.connector_only = True
+
+    model = auto_mixed_precision(model, amp_level="O2", dtype=ms.float16)
+    print(f"=====> Building model done.")
+
+    # loading checkpoint
+    checkpoint_path = args.checkpoint_path if args.checkpoint_path.lower() not in ("", "none") else None
+    if checkpoint_path is not None:
+        s_time = time.time()
+        print(f"checkpoint loading...")
+        state_dict = ms.load_checkpoint(checkpoint_path)
+        m, u = ms.load_param_into_net(model, state_dict)
+
+        m = [n for n in m if ".inv_freq" not in n]
+        if len(m) > 0:
+            print(f"WARNING: missing keys num: {len(m)}, top 10 name is: {m[:10]}")
+        if len(u) > 0:
+            print(f"WARNING: unexpected keys num: {len(u)}, top 10 name is: {u[:10]}")
+
+        print(f"load checkpoint from `{checkpoint_path}` success, time cost: {time.time() - s_time:.2f}s")
+    else:
+        print(f"WARNING: No available pre-trained weights")
+
+    while True:
+        prompt = input("Enter your prompt [e.g. `What's your name?`] or enter [`q`] to exit: ")
+
+        if prompt == "q":
+            print("Generate task done, see you next time!")
+            break
+
+        prompt = [prompt,]
+        input_ids = np.array(tokenizer(prompt).input_ids)
+
+        # input_ids = ms.Tensor(input_ids, ms.int32)
+        # input_kwargs = {}
+        # if args.use_embed_input:
+        #     input_kwargs["inputs_embeds"] = model.get_input_embeddings()(input_ids)
+        # else:
+        #     input_kwargs["input_ids"] = input_ids
+
+        output_ids = model.generate(
+            input_ids,
+            images=None,
+            use_cache=False,
+            max_new_tokens=30,
+            do_sample=False,
+        )
+        # output_ids = output_ids.asnumpy()
+
+        outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0].strip()
+
+        print(f"=====> input prompt: {prompt}, time cost: {time.time() - s_time:.2f}s")
+        print("=" * 46 + " Result " + "=" * 46)
+        print(outputs)
+        print("=" * 100)
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="test")
